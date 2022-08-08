2280b3e276f39b2c0d02d38aec341a74caf85091
Mikhail Lyubeznov <xelysqgbhju@gmail.com>
2022-08-08 13:05:52 +0300

    test

--- src/test/java/tests/ReqresTests.java
+++ src/test/java/tests/ReqresTests.java
@@ -1,4 +1,30 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+
 public class ReqresTests {
+
+    @Test
+    void listUsers () {
+
+        String body = "{ \"data\": { \"id\": 2, \"email\": \"janet.weaver@reqres.in\", \"first_name\": \"Janet\", " +
+                "\"last_name\": \"Weaver\", \"avatar\": \"https://reqres.in/img/faces/2-image.jpg\" }, \"support\": " +
+                "{ \"url\": \"https://reqres.in/#support-heading\", \"text\": \"To keep ReqRes free, contributions " +
+                "towards server costs are appreciated!\" } }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body();
+    }
 }
